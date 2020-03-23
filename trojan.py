1abb70c7cea929464055bb42c5f584348228913e
Ormorod <Adam.Ormondroyd@gmail.com>
2020-03-23 16:59:23 +0000

    Changed to LSODA
    Changed integration method to LSODA. Also changed OMEGA to W

--- trojan.py
+++ trojan.py
@@ -7,18 +7,18 @@ M_J = 0.001
 R = 5.2
 R_SUN = R * M_J / (M_J + M_SUN)
 R_J = R * M_SUN / (M_J + M_SUN)
-OMEGA = 2 * np.pi * (M_J + M_SUN) / R ** (3 / 2)
+W = 2 * np.pi * (M_J + M_SUN) ** (1 / 2) / R ** (3 / 2)  # angular velocity
 G = 4 * np.pi ** 2
 
 
 def r_sun(t):
     """Position of the sun at time t"""
-    return np.array([-R_SUN * np.cos(OMEGA * t), -R_SUN * np.sin(OMEGA * t), 0])
+    return np.array([-R_SUN * np.cos(W * t), -R_SUN * np.sin(W * t), 0])
 
 
 def r_j(t):
     """Position of Jupiter at time t"""
-    return np.array([R_J * np.cos(OMEGA * t), R_J * np.sin(OMEGA * t), 0])
+    return np.array([R_J * np.cos(W * t), R_J * np.sin(W * t), 0])
 
 
 def acceleration(t, r):
@@ -30,24 +30,25 @@ def acceleration(t, r):
 
 
 def f(t, y):
-    """Position of asteroid for solver"""
+    """Position and velocity of asteroid for solver"""
     return np.hstack((y[3:6], acceleration(t, y[0:3])))
 
 
-def asteroid(run_time, t_eval, R0=R_J, theta0=np.pi / 3):
+def asteroid(run_time, t_eval):
     """Trajectory of asteroid calculated using solve_ivp"""
+    theta0 = np.pi / 3
     y0 = np.array(
         [
-            R0 * np.cos(theta0),
-            R0 * np.sin(theta0),
+            R * np.cos(theta0) - R_SUN,
+            R * np.sin(theta0),
             0,
-            -OMEGA * R0 * np.sin(theta0),
-            OMEGA * R0 * np.cos(theta0),
+            -W * R * np.sin(theta0),
+            W * (R * np.cos(theta0)),
             0,
         ]
     )
     print(y0[0])
-    return solve_ivp(f, (0, run_time), y0, t_eval=t_eval)
+    return solve_ivp(f, (0, run_time), y0, t_eval=t_eval, method="LSODA")
 
 
 end_time = 100
