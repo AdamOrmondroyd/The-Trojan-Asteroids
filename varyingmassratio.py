a755783c52614db7bb7a2d4f5ff6843d004c7acf
Ormorod <Adam.Ormondroyd@gmail.com>
2020-04-17 19:06:13 +0100

    Update varyingmassratio.py
    Made varyingmassratio actually work

--- varyingmassratio.py
+++ varyingmassratio.py
@@ -1,29 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import pi
 import rotatingframe
-import constants
-from rotatingframe import asteroid, max_wander
-from constants import L4, T
 
-end_time = 100 * T
-points_per_year = 100
-ts = np.linspace(0, end_time, int(end_time * points_per_year))
 
-print(max_wander(end_time, ts, r_0=L4, v_0=np.array([0, 0, 0]), stability_point=L4,))
-
-m_min = 0.0157
-m_max = 0.0166
+m_min = 0.0
+m_max = 0.02
 points = 100
 ms = np.linspace(m_min, m_max, points)
-
 wanders = np.zeros(points)
+
+import constants
 
 for i in range(points):
     print(i)
+    constants.M_SUN = 1
     constants.M_J = ms[i]
-    rotatingframe.M_J = ms[i]
+    constants.R = 5.2
+    constants.R_SUN = constants.R * ms[i] / (ms[i] + constants.M_SUN)
+    constants.R_J = constants.R * constants.M_SUN / (ms[i] + constants.M_SUN)
+    constants.W = 2 * pi * (ms[i] + constants.M_SUN) ** (1 / 2) / constants.R ** (3 / 2)
+    constants.T = 2 * pi / constants.W
+
+    constants.L4 = np.array(
+        [constants.R / 2 - constants.R_SUN, constants.R * np.sqrt(3) / 2, 0]
+    )
+    constants.L5 = np.array(
+        [constants.R / 2 - constants.R_SUN, -constants.R * np.sqrt(3) / 2, 0]
+    )
+
+    from rotatingframe import max_wander
+
+    end_time = 100 * constants.T
+    points_per_year = 100
+    ts = np.linspace(0, end_time, int(end_time * points_per_year))
+
     wanders[i] = max_wander(
-        end_time, ts, r_0=L4, v_0=np.array([0, 0, 0]), stability_point=L4,
+        end_time,
+        ts,
+        r_0=constants.L4,
+        v_0=np.array([0, 0, 0]),
+        stability_point=constants.L4,
     )
 
 plt.plot(ms, wanders)
