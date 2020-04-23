7b6f165cf7d391bc4419ee429fb4f01cfe97f087
Ormorod <Adam.Ormondroyd@gmail.com>
2020-04-23 22:17:50 +0100

    Revert "Changed rotating asteroid to oop"
    This reverts commit f11a87a01502a2e42189114aca313e76b80763b8.

--- varyingmassratio.py
+++ varyingmassratio.py
@@ -1,10 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from rotatingframe import asteroid
-import time
-import multiprocessing
+from numpy import pi
+import rotatingframe
 from scipy.optimize import curve_fit
-
 
 ### Starts to die at around m=0.36
 m_min = 0.001
@@ -13,53 +11,66 @@ points = 100
 ms = np.linspace(m_min, m_max, points)
 wanders = np.zeros(points)
 
+import constants
 
-def max_wander_wrapper(m):
-    ast = asteroid(M_J=m)
-    end_time = 100 * ast.T
+for i in range(points):
+    print(i)
+    constants.M_SUN = 1
+    constants.M_J = ms[i]
+    constants.R = 5.2
+    constants.R_SUN = constants.R * ms[i] / (ms[i] + constants.M_SUN)
+    constants.R_J = constants.R * constants.M_SUN / (ms[i] + constants.M_SUN)
+    constants.W = 2 * pi * (ms[i] + constants.M_SUN) ** (1 / 2) / constants.R ** (3 / 2)
+    constants.T = 2 * pi / constants.W
+
+    constants.L4 = np.array(
+        [constants.R / 2 - constants.R_SUN, constants.R * np.sqrt(3) / 2, 0]
+    )
+    constants.L5 = np.array(
+        [constants.R / 2 - constants.R_SUN, -constants.R * np.sqrt(3) / 2, 0]
+    )
+
+    from rotatingframe import max_wander
+
+    end_time = 100 * constants.T
     points_per_year = 100
     ts = np.linspace(0, end_time, int(end_time * points_per_year))
-    return ast.max_wander(
-        ts, r_0=ast.L4, v_0=np.array([0, 0, 0]), stability_point=ast.L4,
+
+    wanders[i] = max_wander(
+        ts, r_0=constants.L4, v_0=np.array([0, 0, 0]), stability_point=constants.L4,
     )
 
 
-if __name__ == "__main__":
-    # wanders = np.zeros(points)
-    # for i in range(points):
-    #     wanders[i] = max_wander_wrapper(ms[i])
-    pool = multiprocessing.Pool(processes=1)
-    wanders = pool.map(max_wander_wrapper, ms)
-    pool.close()
+def quadratic(x, a, b, c):
+    """Quadratic for curve fit"""
+    return a * x ** 2 + b * x + c
 
-    def quadratic(x, a, b, c):
-        """Quadratic for curve fit"""
-        return a * x ** 2 + b * x + c
 
-    def linear(x, a, b):
-        return a * x + b
+def linear(x, a, b):
+    return a * x + b
 
-    (a, b, c), pcov = curve_fit(quadratic, ms, wanders)
 
-    print("a: " + str(a))
-    print("b: " + str(b))
-    print("c: " + str(c))
+(a, b, c), pcov = curve_fit(quadratic, ms, wanders)
 
-    (d, e), pcov2 = curve_fit(linear, ms, wanders)
+print("a: " + str(a))
+print("b: " + str(b))
+print("c: " + str(c))
 
-    print("d: " + str(d))
-    print("e: " + str(e))
+(d, e), pcov2 = curve_fit(linear, ms, wanders)
 
-    fig, ax = plt.subplots()
+print("d: " + str(d))
+print("e: " + str(e))
 
-    ax.plot(ms, wanders, label="wanders", marker="+", linestyle="None")
-    ax.plot(ms, quadratic(ms, a, b, c), label="quadratic fit")
+fig, ax = plt.subplots()
 
-    ax.set(
-        title="Varying the mass ratio",
-        xlabel="M$_J$/M$_{\mathrm{SUN}}$",
-        ylabel="Maximum wander / au",
-    )
-    ax.legend()
-    plt.savefig("mass_wanders.png")
-    plt.show()
+ax.plot(ms, wanders, label="wanders", marker="+", linestyle="None")
+ax.plot(ms, quadratic(ms, a, b, c), label="quadratic fit")
+
+ax.set(
+    title="Varying the mass ratio",
+    xlabel="M$_J$/M$_{\mathrm{SUN}}$",
+    ylabel="Maximum wander / au",
+)
+ax.legend()
+plt.savefig("mass.png")
+plt.show()
