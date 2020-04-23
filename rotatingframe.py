fdb6d88353ac2ab8c975f23e23ff6167e6f9c18c
Ormorod <Adam.Ormondroyd@gmail.com>
2020-04-23 22:33:17 +0100

    Revert "Revert "Revert "Changed rotating asteroid to oop"""
    This reverts commit 6d481597f970c0cf7e4161f4a2547836a25727d3.

--- rotatingframe.py
+++ rotatingframe.py
@@ -1,85 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
-from constants import G, M_SUN
+from constants import G, M_SUN, M_J, R, R_SUN, R_J, W
+
+r_sun = np.array([-R_SUN, 0, 0])
+r_j = np.array([R_J, 0, 0])
 
 
-class asteroid:
-    def __init__(self, M_J=0.001, R=5.2):
-        self._M_J = M_J
-        self._R = R
+def acceleration(t, r, v):
+    """Acceleration of an asteroid at position r and time t"""
+    return -G * (
+        M_SUN * (r - r_sun) / np.linalg.norm(r - r_sun) ** 3
+        + M_J * (r - r_j) / np.linalg.norm(r - r_j) ** 3
+    ) + W * np.array(
+        [2 * v[1] + W * r[0], -2 * v[0] + W * r[1], 0]
+    )  # added coriolis and centrifugal forces
 
-        self._R_SUN = R * self._M_J / (self._M_J + M_SUN)
-        self._R_J = R * M_SUN / (self._M_J + M_SUN)
-        self._r_sun = np.array([-self._R_SUN, 0, 0])
-        self._r_j = np.array([self._R_J, 0, 0])
 
-        self._W = np.sqrt(G * (self._M_J + M_SUN) / R ** 3)
-        self._T = 2 * np.pi / self._W
+def derivs(t, y):
+    """derivatives for solver"""
+    return np.hstack((y[3:6], acceleration(t, y[0:3], y[3:6])))
 
-        self._L4 = np.array([self._R / 2 - self._R_SUN, self._R * np.sqrt(3) / 2, 0])
-        self._L5 = np.array([self._R / 2 - self._R_SUN, -self._R * np.sqrt(3) / 2, 0])
 
-    @property
-    def R(self):
-        """Get distance between Sun and Jupiter"""
-        return self._R
+def asteroid(t_eval, r_0, v_0):
+    """Trajectory of asteroid calculated using solve_ivp"""
+    y0 = np.append(r_0, v_0)
 
-    @property
-    def R_SUN(self):
-        """Get distance from origin to Sun"""
-        return self._R_SUN
+    return solve_ivp(derivs, (0, t_eval[-1]), y0, t_eval=t_eval, method="LSODA")
 
-    @property
-    def R_J(self):
-        """Get distance from origin to Jupiter"""
-        return self._R_J
 
-    @property
-    def W(self):
-        """Get angular frequency"""
-        return self._W
-
-    @property
-    def T(self):
-        """Get time period"""
-        return self._T
-
-    @property
-    def L4(self):
-        """Get L4"""
-        return self._L4
-
-    @property
-    def L5(self):
-        """Get L5"""
-        return self._L5
-
-    def trajectory(self, t_eval, r_0, v_0):
-        """Trajectory of asteroid calculated using solve_ivp"""
-        y0 = np.append(r_0, v_0)
-
-        def _acceleration(t, r, v):
-            """Acceleration of an asteroid at position r with velocity v at time t"""
-            return -G * (
-                M_SUN * (r - self._r_sun) / np.linalg.norm(r - self._r_sun) ** 3
-                + self._M_J * (r - self._r_j) / np.linalg.norm(r - self._r_j) ** 3
-            ) + self._W * np.array(
-                [2 * v[1] + self._W * r[0], -2 * v[0] + self._W * r[1], 0]
-            )  # added coriolis and centrifugal forces
-
-        def _derivs(t, y):
-            """derivatives for solver"""
-            return np.hstack((y[3:6], _acceleration(t, y[0:3], y[3:6])))
-
-        return solve_ivp(_derivs, (0, t_eval[-1]), y0, t_eval=t_eval, method="LSODA")
-
-    def max_wander(self, t_eval, r_0, v_0, stability_point):
-        """Find the maximum distance from the starting point for given initial conditions in the rotating frame"""
-        sol = self.trajectory(t_eval, r_0, v_0)
-        rs = sol.y[0:3]  # extract positions from solution
-        deltas = (
-            rs.T - stability_point
-        ).T  # Transpose used to stick to array convention
-        norms = np.linalg.norm(deltas, axis=0)
-        return norms.max()
+def max_wander(t_eval, r_0, v_0, stability_point):
+    """Find the maximum distance from the starting point for given initial conditions in the rotating frame"""
+    sol = asteroid(t_eval, r_0, v_0)
+    rs = sol.y[0:3]  # extract positions from solution
+    deltas = (rs.T - stability_point).T  # Transpose used to stick to array convention
+    norms = np.linalg.norm(deltas, axis=1)
+    return norms.max()
--- staticplot.py
+++ staticplot.py
@@ -1,34 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from rotatingframe import asteroid
-
-# from constants import L4, L5, R, R_SUN, R_J
+from constants import L4, L5, R, R_SUN, R_J
 
 end_time = 100
 points_per_year = 1000
 ts = np.linspace(0, end_time, int(end_time * points_per_year))
 
-ast = asteroid()
-
-sol = ast.trajectory(t_eval=ts, r_0=ast.L4, v_0=np.array([0, 0, 0]))
+sol = asteroid(t_eval=ts, r_0=L4, v_0=np.array([0, 0, 0]))
 
 plt.plot(
-    -ast.R_SUN,
-    0,
-    label="sun",
-    color="orange",
-    marker="*",
-    markersize=20,
-    linestyle="None",
+    -R_SUN, 0, label="sun", color="orange", marker="*", markersize=20, linestyle="None"
 )
 plt.plot(
-    ast.R_J,
-    0,
-    label="Jupiter",
-    color="red",
-    marker="o",
-    markersize=10,
-    linestyle="None",
+    R_J, 0, label="Jupiter", color="red", marker="o", markersize=10, linestyle="None"
 )
 plt.plot(sol.y[0], sol.y[1], "-", label="Greeks", color="green")
 plt.xlabel("x/au")
